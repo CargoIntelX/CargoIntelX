9975b7fdfb64f3c0d6afd660f231e99ca5f7d9a7
Mithun <sgmithun24@gmail.com>
2026-05-10 10:08:45 +0530

    Creating Repo

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import daisyui from "daisyui";
+
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [daisyui],
+
+  daisyui: {
+    themes: ["light"],
+  },
+};
+
+export default config;
